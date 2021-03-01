671bc6345eb98742dfd167a314ceff26a7ec9009
Javier ConRod <javierin_kl@hotmail.com>
2021-03-01 10:47:40 -0700

    Model!

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\support\Facades\DB;
+use App\Models\Notas;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,7 +20,7 @@ Route::get('/', function () {
 
 Route::get('notas', function(){
     
-    $notas = DB::table('notas')->get();
+    $notas = Notas::all(); //DB::table('notas')->get();
 
     return view('notas', ['notas' => $notas]);
 })->name('notas.index');
@@ -29,9 +30,7 @@ Route::get('agregar', function(){
 });
 
 Route::get('notas/{id}/editar', function($id){
-    $notas = DB::table('notas')
-    ->where('id', $id)
-    ->first();
+    $notas = Notas::find($id);//DB::table('notas') ->where('id', $id) ->first();
 
     return view('editar', ['notas' => $notas]);
 })->name('notas.edit');
